b2b747a3dd55ff2be234445a667ec6ee7a0287ab
LeoniUwase <leoniuwase11@gmail.com>
2025-07-12 18:51:44 +0200

    Add files via upload

---
+++ Covid Portfolio Project Script.sql
@@ -0,0 +1,12 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [portfolio project ].[dbo].[Percent_population_vaccinated]
+
+
+
+  SELECT  *
+  FROM Percent_population_vaccinated 
